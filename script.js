906e52ab392035f12ab93c0fc63601d631f99d79
frogpad <136676079+frogrin@users.noreply.github.com>
2023-08-04 14:24:03 +0300

    Add functions that do basic math operations

---
+++ script.js
@@ -0,0 +1,20 @@
+// Create functions for addition, subtraction, multiplication, division
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
+
+let firstNumber = 3;
+let secondNumber = 2;
+let operator = '+';
