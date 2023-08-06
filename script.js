4eb51a101e739294b1ee8843a554123b43eb15b7
Frog <136676079+frogrin@users.noreply.github.com>
2023-08-06 16:18:55 +0300

    Add basic functionality

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Create functions for addition, subtraction, multiplication, division
 function add(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
 }
@@ -15,9 +14,9 @@ function divide(firstNumber, secondNumber) {
   return firstNumber / secondNumber;
 }
 
-let firstNumber = 320;
-let secondNumber = 2;
-let operator = '/';
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
 
 function operate(operator, firstNumber, secondNumber) {
   switch (operator) {
@@ -35,3 +34,45 @@ function operate(operator, firstNumber, secondNumber) {
       break;
   }
 }
+
+const display = document.querySelector('.display');
+let displayValue = '';
+display.textContent = displayValue;
+let currentNumber = '';
+
+function updateDisplay(value) {
+  displayValue += value;
+  display.textContent = displayValue;
+}
+
+// Add event listeners to number buttons
+const numbers = document.querySelectorAll('.numbers button');
+
+for (const number of numbers) {
+  if (number.textContent === '=') continue;
+  number.addEventListener('click', () => {
+    currentNumber += number.textContent;
+    updateDisplay(number.textContent);
+  });
+}
+
+// Add event listeners to operator buttons
+const operators = document.querySelectorAll('.operators button');
+
+for (const opr of operators) {
+  if (opr.textContent === 'C') continue;
+  opr.addEventListener('click', () => {
+  operator = opr.textContent;
+  updateDisplay(` ${operator} `);
+  firstNumber = Number(currentNumber);
+  currentNumber = '';
+  });
+
+}
+
+// Add event listener for equals button
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', () => {
+  secondNumber = Number(currentNumber);
+  display.textContent = operate(operator, firstNumber, secondNumber);
+});
